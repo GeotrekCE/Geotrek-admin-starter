67b31af42dfb79281b95417afbfe11bb7d73497e
J-E Castagnede <j.e.castagnede@gmail.com>
2018-06-13 17:09:45 +0200

    service customization

--- backup.sh
+++ backup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# must be run inside docker-compose directory
+cd /home/arche-agglo/arche-agglo-admin
 
 source ./.env
 export PGPASSWORD=$(echo $POSTGRES_PASSWORD)
 pg_dump -Fc --no-acl --no-owner -h 127.0.0.1 -U $POSTGRES_USER $POSTGRES_DB > geotrek_`date +\%Y-\%m-\%d_\%H:\%M`.dump
 
-tar --exclude='*.djcache'  --exclude='*.tgz' --exclude='*.log' -zcvf /var/backups/geotrek_`date +\%Y-\%m-\%d_\%H:\%M`.tgz .
+tar --exclude='*.djcache'  --exclude='*.tgz' --exclude='*.log' -zcvf /var/backups/geotrek/arche-agglo/geotrek_`date +\%Y-\%m-\%d_\%H:\%M`.tgz .
 rm geotrek_`date +\%Y-\%m-\%d_\%H:\%M`.dump
