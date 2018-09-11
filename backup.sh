5d16694a10ab02d6c6b0a9d23c23475fd14b529d
J-E Castagnede <j.e.castagnede@gmail.com>
2018-09-11 12:19:15 +0200

    fix conf

--- backup.sh
+++ backup.sh
@@ -9,4 +9,4 @@ pg_dump -Fc --no-acl --no-owner -h 127.0.0.1 -U $POSTGRES_USER $POSTGRES_DB > ge
 mkdir -p /var/backups/geotrek/$(basename $PWD)
 
 tar --exclude='*.djcache'  --exclude='*.tgz' --exclude='*.log' --exclude='*.dump' --exclude='./var/data/*' --exclude='./var/static/*' --exclude='./var/tiles/*' -zcvf /var/backups/geotrek/$(basename $DIR)/geotrek_`date +\%Y-\%m-\%d_\%H:\%M`.tgz .
-rm geotrek_`date +\%Y-\%m-\%d_\%H:\%M`.dump
+rm ./geotrek_*.dump
