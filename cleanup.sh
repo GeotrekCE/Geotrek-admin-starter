e37e0d859f7beac9def006609ac662c818706eb9
J-E Castagnede <j.e.castagnede@gmail.com>
2018-09-11 12:18:27 +0200

    fix conf

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PWD="$(pwd)"
+DIR="$(dirname "$0")"
 
-docker exec $(docker ps -q -f name="$(basename $PWD)"_web) ./manage.py clearsessions
-docker exec $(docker ps -q -f name="$(basename $PWD)"_web) ./manage.py thumbnail_cleanup
+docker exec $(docker ps -q -f name="$(basename $DIR)"_web) ./manage.py clearsessions
+docker exec $(docker ps -q -f name="$(basename $DIR)"_web) ./manage.py thumbnail_cleanup
--- sync_rando.sh
+++ sync_rando.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
+DIR="$(dirname "$0")"
 
-cd $(pwd)
-
-docker exec $(docker ps -q -f name=$(pwd)_web) ./manage.py sync_rando -v2 /app/src/var/data
+docker exec $(docker ps -q -f name=$(basename $DIR)_web) ./manage.py sync_rando -v2 /app/src/var/data
