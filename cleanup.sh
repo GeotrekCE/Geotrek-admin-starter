3eb47171d64505d8630010fea364d931a941864f
J-E Castagnede <j.e.castagnede@gmail.com>
2018-11-13 13:10:04 +0100

    fix std backup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-DIR="$(dirname "$0")"
-
-docker exec $(docker ps -q -f name="$(basename $DIR)"_web) ./manage.py clearsessions
-docker exec $(docker ps -q -f name="$(basename $DIR)"_web) ./manage.py thumbnail_cleanup
+docker-compose exec web ./manage.py clearsessions
+docker-compose exec web ./manage.py thumbnail_cleanup
--- sync_rando.sh
+++ sync_rando.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-DIR="$(dirname "$0")"
 
 # take care to configure SYNC_RANDO_OPTIONS in custom.py
-docker exec $(docker ps -q -f name=$(basename $DIR)_web) ./manage.py sync_rando -v2 /app/src/var/data
+docker-compose exec web ./manage.py sync_rando -v2 /app/src/var/data
