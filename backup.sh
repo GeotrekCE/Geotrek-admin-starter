ce7579c99afc8e0b0133218ac3b4117cf3bae65b
J-E Castagnede <j.e.castagnede@gmail.com>
2019-03-25 17:44:10 +0100

    fix good backup path

--- backup.sh
+++ backup.sh
@@ -3,11 +3,11 @@
 DIR="$(dirname "$0")"
 cd $DIR
 
-mkdir -p /var/backups/geotrek/$(basename $PWD)
+mkdir -p /mnt/backups/geotrek/$(basename $PWD)
 
 source ./.env
 export PGPASSWORD=$(echo $POSTGRES_PASSWORD)
 pg_dump -Fc --no-acl --no-owner -h 127.0.0.1 -U $POSTGRES_USER $POSTGRES_DB > geotrek_`date +\%Y-\%m-\%d_\%H-\%M`.dump
 
-tar --exclude='*.djcache' --exclude='.git/*' --exclude='*.tar.bz2' --exclude='*.log' --exclude='./var/data/*' --exclude='./var/static/*' --exclude='./var/tiles/*' -cavf /var/backups/geotrek/$(basename $PWD)/geotrek_`date +\%Y-\%m-\%d_\%H-\%M`.tar.bz2 .
+tar --exclude='*.djcache' --exclude='.git/*' --exclude='*.tar.bz2' --exclude='*.log' --exclude='./var/data/*' --exclude='./var/static/*' --exclude='./var/tiles/*' -cavf /mnt/backups/geotrek/$(basename $PWD)/geotrek_`date +\%Y-\%m-\%d_\%H-\%M`.tar.bz2 .
 rm ./geotrek_*.dump
